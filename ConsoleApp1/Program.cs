a88b84f1e865a19ca6c9055b20386bc7764445c9
HoangAnh <nguyehoanganh8976@gmail.com>
2021-12-04 11:20:45 +0700

    Bai code C

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+int main()
+{
+    int i, n;
+    long S;
+    S = 0;
+    i = 1;
+    PriorityQueue
+    object p1 = SendOrPostCallback(("%d", &n);  
+    object p = SendOrPostCallback(("%d", &n);
+
+
+    while (i <= n)
+    {
+        S = S + i;
+        i++;
+    }
+    printf("\nTong 1 + 2 + ... + %d la %ld: ", n, S);
+    GetType();
+    return 0;
+}
